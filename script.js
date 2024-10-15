52b95a43859ab3b219640ef161ae0c84893126d9
Ramzicarspalace <ramzicarpalace@gmail.com>
2024-10-15 20:15:06 +0300

    added project files

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+    alert('Thank you for your message! We will get back to you soon.'); // Alert on form submission
+});
